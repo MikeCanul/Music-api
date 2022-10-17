d22f5c55af801663aa9d3f204fcad4ca963c61e4
MikeCanul <mike.canuljimenez@cbtis72.edu.mx>
2022-10-17 14:36:08 -0500

    Commit 3
    Commit 3 video 9 carpeta 15

--- src/main/java/net/itinajero/api/service/jpa/AlbumsService.java
+++ src/main/java/net/itinajero/api/service/jpa/AlbumsService.java
@@ -1,10 +1,8 @@
 package net.itinajero.api.service.jpa;
 
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import net.itinajero.api.entity.Album;
 import net.itinajero.api.repository.AlbumsRepository;
 import net.itinajero.api.service.IAlbumsService;
@@ -14,10 +12,17 @@ public class AlbumsService implements IAlbumsService {
 
 	@Autowired
 	private AlbumsRepository repoAlbums;
-	@Override
+	
 	public List<Album> buscarTodos() {
-		// TODO Auto-generated method stub
 		return repoAlbums.findAll();
+	}
+
+	public void guardar(Album album) {
+		repoAlbums.save(album);
+	}
+
+	public void eliminar(int idAlbum) {
+		repoAlbums.deleteById(idAlbum);
 	}
 
 }
